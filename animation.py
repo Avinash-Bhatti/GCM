75c082d5786d616b26552d1a0c41a7f4ff7b394e
HendoGit <72992029+HendoGit@users.noreply.github.com>
2021-02-01 22:34:35 +0000

    changed grid locations n added random colours

--- animation.py
+++ animation.py
@@ -24,12 +24,12 @@ writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
 
 fig2 = plt.figure()
 
-x = np.arange(0, 10)
-y = np.arange(0, 10).reshape(-1, 1)
+x = np.arange(-0.5, 4.5, 1)
+y = np.arange(-0.5, 4.5, 1).reshape(-1, 1)
 base = np.hypot(x, y)
 ims = []
 for add in np.arange(35):
-    ims.append((plt.pcolor(x, y, base + add, norm=plt.Normalize(0, 30)),))
+    ims.append((plt.pcolor(x, y, np.random.rand(4,4), norm=plt.Normalize(0, 1)),))
 
 im_ani = animation.ArtistAnimation(fig2, ims, interval=200, repeat_delay=3000,
                                    blit=True)
